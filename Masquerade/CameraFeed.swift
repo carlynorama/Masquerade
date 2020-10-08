5883ca56e3b0c5a5b0abf8a2f4730655adeaa08d
Carlyn Maw <carlynorama@gmail.com>
2020-10-07 17:01:56 -0700

    UIView with message

--- Masquerade/CameraFeed.swift
+++ Masquerade/CameraFeed.swift
@@ -9,17 +9,48 @@ import SwiftUI
 import AVFoundation
 
 struct CameraFeed:UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
     
     class CameraCoordinator: NSObject, UINavigationControllerDelegate {
         var parent: CameraFeed
         init(parent: CameraFeed) {
             self.parent = parent
         }
+
     }
 
     class CameraViewController:UIViewController {
         var delegate: CameraCoordinator?
-       // UIImagePickerController()
+        override public func loadView() {
+            view = UIView()
+            view.isUserInteractionEnabled = true
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.numberOfLines = 0
+            
+            label.text = "This is a UIView. Tap anywhere to dismiss."
+            label.textAlignment = .center
+            
+            let stackView = UIStackView()
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            stackView.axis = .vertical
+            stackView.spacing = 50
+            stackView.addArrangedSubview(label)
+            //stackView.addArrangedSubview(button)
+            
+            view.addSubview(stackView)
+            
+            NSLayoutConstraint.activate([
+                //button.heightAnchor.constraint(equalToConstant: 50),
+                stackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+                stackView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+                stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
+            
+        }
+        
+        override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.dismiss(animated: true, completion: nil)        }
     }
     
     public func makeCoordinator() -> CameraCoordinator {
