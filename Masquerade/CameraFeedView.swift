97ff2936c776153f7760260fafc49459a6c52beb
carlynorama <carlynorama@gmail.com>
2020-10-07 18:09:41 -0700

    formatting

--- Masquerade/CameraFeedView.swift
+++ Masquerade/CameraFeedView.swift
@@ -9,22 +9,22 @@ import SwiftUI
 
 struct CameraFeedView: View {
     @State private var image: Image?
-       @State private var showingUIView = false
-
-       var body: some View {
-           VStack {
-               image?
-                   .resizable()
-                   .scaledToFit()
-
-               Button("Pull Up UIView") {
-                  self.showingUIView = true
-               }
-           }
-           .sheet(isPresented: $showingUIView) {
-               CameraFeed()
-           }
-       }
+    @State private var showingUIView = false
+    
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Pull Up UIView") {
+                self.showingUIView = true
+            }
+        }
+        .sheet(isPresented: $showingUIView) {
+            CameraFeed()
+        }
+    }
 }
 
 struct CameraFeedView_Previews: PreviewProvider {
